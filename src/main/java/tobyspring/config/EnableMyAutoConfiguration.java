5473d270c29a966809e394f9c3a7964caaacc5c4
robb <robb@chequer.io>
2024-09-05 00:36:08 +0900

    study section 7-3

---
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,16 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.Import;
+import tobyspring.config.autoconfig.DispatcherServletConfig;
+import tobyspring.config.autoconfig.TomcatWebServerConfiguration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DispatcherServletConfig.class, TomcatWebServerConfiguration.class})
+public @interface EnableMyAutoConfiguration {
+}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,9 +1,10 @@
 package tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
+import tobyspring.config.MySpringBootApplication;
 
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class HellobootApplication {
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
