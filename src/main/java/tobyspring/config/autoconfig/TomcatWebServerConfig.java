f485378088b1c64664bbf4f2107e1e9f1543b08b
robb <mul501project@gmail.com>
2024-09-23 23:49:38 +0900

    section7 파일 정리

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -3,20 +3,17 @@ package tobyspring.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-<<<<<<<< HEAD:src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+import org.springframework.context.annotation.Configuration;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
 
-========
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TomcatWebServerConfiguration {
->>>>>>>> 5473d270c29a966809e394f9c3a7964caaacc5c4:src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfiguration.java
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    @Configuration
+    public class TomcatWebServerConfiguration {
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory() {
+            return new TomcatServletWebServerFactory();
+        }
     }
 }
