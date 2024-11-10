47119712799f4f885b8418eaaa3c0af01af297c7
robb <mul501project@gmail.com>
2024-11-10 18:32:32 +0900

    study section 8-3

--- src/test/java/tobyspring/study/ConditionalTest.java
+++ src/test/java/tobyspring/study/ConditionalTest.java
@@ -1,15 +1,46 @@
 package tobyspring.study;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConditionalTest {
     @Test
     void conditional() {
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+
+        ApplicationContextRunner contextRunner2 = new ApplicationContextRunner();
+        contextRunner2.withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                    assertThat(context).doesNotHaveBean(Config2.class);
+                });
+
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(BooleanCondition.class)
+    @interface BooleanConditional {
+        boolean value();
     }
 
     @Configuration
+    @BooleanConditional(true)
     static class Config1 {
         @Bean
         MyBean myBean() {
@@ -18,6 +49,7 @@ public class ConditionalTest {
     }
 
     @Configuration
+    @BooleanConditional(false)
     static class Config2 {
         @Bean
         MyBean myBean() {
@@ -28,4 +60,14 @@ public class ConditionalTest {
 
     static class MyBean {
     }
+
+    private static class BooleanCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            Map<String, Object> annotationAttributes = metadata.getAnnotationAttributes(BooleanConditional.class.getName());
+            Boolean value = (Boolean) annotationAttributes.get("value");
+            return value;
+        }
+    }
+
 }
